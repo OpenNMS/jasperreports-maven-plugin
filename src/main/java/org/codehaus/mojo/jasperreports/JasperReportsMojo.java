c61b8c5545c1e96c41a372369fbe30896eab2763
dantran <dantran@52ab4f32-60fc-0310-b215-8acea882cd1b>
2008-11-13 06:35:01 +0000

    Move the setting of JRProperties after loading the internal classloader
    git-svn-id: http://svn.codehaus.org/mojo/trunk/mojo/jasperreports-maven-plugin@8081 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/main/java/org/codehaus/mojo/jasperreports/JasperReportsMojo.java
+++ src/main/java/org/codehaus/mojo/jasperreports/JasperReportsMojo.java
@@ -1,22 +1,18 @@
 package org.codehaus.mojo.jasperreports;
 
 /*
- *  Licensed to the Apache Software Foundation (ASF) under one
- *  or more contributor license agreements.  See the NOTICE file
- *  distributed with this work for additional information
- *  regarding copyright ownership.  The ASF licenses this file
- *  to you under the Apache License, Version 2.0 (the
- *  "License") you may not use this file except in compliance
- *  with the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License") you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 import net.sf.jasperreports.engine.JasperCompileManager;
@@ -43,9 +39,11 @@ import java.util.Set;
 
 /**
  * Compiles JasperReports xml definition files.
- * <p>Much of this was inspired by the JRAntCompileTask, while trying to make it
- * slightly cleaner and easier to use with Maven's mojo api.</p>
- *
+ * <p>
+ * Much of this was inspired by the JRAntCompileTask, while trying to make it slightly cleaner and
+ * easier to use with Maven's mojo api.
+ * </p>
+ * 
  * @author gjoseph
  * @author Tom Schwenk
  * @goal compile-reports
@@ -62,61 +60,57 @@ public class JasperReportsMojo
 
     /**
      * This is where the generated java sources are stored.
-     *
+     * 
      * @parameter expression="${project.build.directory}/jasperreports/java"
      */
     private File javaDirectory;
 
     /**
      * This is where the .jasper files are written.
-     *
+     * 
      * @parameter expression="${project.build.outputDirectory}"
      */
     private File outputDirectory;
 
     /**
      * This is where the xml report design files should be.
-     *
+     * 
      * @parameter default-value="src/main/jasperreports"
      */
     private File sourceDirectory;
 
     /**
-     * The extension of the source files to look for. Finds files
-     * with a .jrxml extension by default.
-     *
+     * The extension of the source files to look for. Finds files with a .jrxml extension by
+     * default.
+     * 
      * @parameter default-value=".jrxml"
      */
     private String sourceFileExt;
 
     /**
-     * The extension of the compiled report files. Creates files
-     * with a .jasper extension by default.
-     *
+     * The extension of the compiled report files. Creates files with a .jasper extension by
+     * default.
+     * 
      * @parameter default-value=".jasper"
      */
     private String outputFileExt;
 
     /**
-     * Since the JasperReports compiler deletes the compiled classes,
-     * one might want to set this to true, if they want to handle
-     * the generated java source in their application.
-     * Mind that this will not work if the mojo is bound to the compile
-     * or any other later phase. (As one might need to do if they use
-     * classes from their project in their report design)
-     *
+     * Since the JasperReports compiler deletes the compiled classes, one might want to set this to
+     * true, if they want to handle the generated java source in their application. Mind that this
+     * will not work if the mojo is bound to the compile or any other later phase. (As one might
+     * need to do if they use classes from their project in their report design)
+     * 
      * @parameter default-value="false"
-     * @deprecated There seems to be an issue with the compiler plugin
-     *             so don't expect this to work yet - the dependencies
-     *             will have disappeared.
+     * @deprecated There seems to be an issue with the compiler plugin so don't expect this to work
+     *             yet - the dependencies will have disappeared.
      */
     private boolean keepJava;
 
     /**
-     * Not used for now - just a TODO - the idea being that one
-     * might want to set this to false if they want to handle the
-     * generated java source in their application.
-     *
+     * Not used for now - just a TODO - the idea being that one might want to set this to false if
+     * they want to handle the generated java source in their application.
+     * 
      * @parameter default-value="true"
      * @deprecated Not implemented
      */
@@ -124,15 +118,15 @@ public class JasperReportsMojo
 
     /**
      * Wether the xml design files must be validated.
-     *
+     * 
      * @parameter default-value="true"
      */
     private boolean xmlValidation;
 
     /**
-     * Uses the Javac compiler by default. This is different from
-     * the original JasperReports ant task, which uses the JDT compiler by default.
-     *
+     * Uses the Javac compiler by default. This is different from the original JasperReports ant
+     * task, which uses the JDT compiler by default.
+     * 
      * @parameter default-value="net.sf.jasperreports.engine.design.JRJavacCompiler"
      */
     private String compiler;
@@ -143,10 +137,9 @@ public class JasperReportsMojo
     private List classpathElements;
 
     /**
-     * Any additional classpath entry you might want to add to
-     * the JasperReports compiler. Not recommended for general use,
-     * plugin dependencies should be used instead.
-     *
+     * Any additional classpath entry you might want to add to the JasperReports compiler. Not
+     * recommended for general use, plugin dependencies should be used instead.
+     * 
      * @parameter
      */
     private String additionalClasspath;
@@ -170,52 +163,47 @@ public class JasperReportsMojo
         checkDir( sourceDirectory, "Source directory", false );
         checkDir( outputDirectory, "Target directory", true );
 
-        JRProperties.backupProperties();
-        try
+        SourceMapping mapping = new SuffixMapping( sourceFileExt, outputFileExt );
+
+        Set staleSources = scanSrcDir( mapping );
+        if ( staleSources.isEmpty() )
         {
-            String classpath = buildClasspathString( classpathElements, additionalClasspath );
-            getLog().debug( "buildClasspathString() = " + classpath );
-            JRProperties.setProperty( JRProperties.COMPILER_CLASSPATH, classpath );
-            JRProperties.setProperty( JRProperties.COMPILER_TEMP_DIR, javaDirectory.getAbsolutePath() );
-            JRProperties.setProperty( JRProperties.COMPILER_KEEP_JAVA_FILE, keepJava );
-            JRProperties.setProperty( JRProperties.COMPILER_CLASS, compiler );
-            JRProperties.setProperty( JRProperties.COMPILER_XML_VALIDATION, xmlValidation );
-
-            SourceMapping mapping = new SuffixMapping( sourceFileExt, outputFileExt );
-
-            Set staleSources = scanSrcDir( mapping );
-            if ( staleSources.isEmpty() )
-            {
-                getLog().info( "Nothing to compile - all Jasper reports are up to date" );
-            }
-            else
-            {
-                // actual compilation
-                compile( staleSources, mapping );
-
-                if ( keepJava )
-                {
-                    project.addCompileSourceRoot( javaDirectory.getAbsolutePath() );
-                }
-            }
+            getLog().info( "Nothing to compile - all Jasper reports are up to date" );
         }
-        finally
+        else
         {
-            JRProperties.restoreProperties();
+            // actual compilation
+            compile( staleSources, mapping );
+
+            if ( keepJava )
+            {
+                project.addCompileSourceRoot( javaDirectory.getAbsolutePath() );
+            }
         }
     }
 
     protected void compile( Set files, SourceMapping mapping )
         throws MojoFailureException, MojoExecutionException
     {
+        String classpath = buildClasspathString( classpathElements, additionalClasspath );
+        getLog().debug( "buildClasspathString() = " + classpath );
+
         getLog().info( "Compiling " + files.size() + " report design files." );
 
         getLog().debug( "Set classloader" );
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         Thread.currentThread().setContextClassLoader( getClassLoader( classLoader ) );
 
+        JRProperties.backupProperties();
+
         try
         {
+            JRProperties.setProperty( JRProperties.COMPILER_CLASSPATH, classpath );
+            JRProperties.setProperty( JRProperties.COMPILER_TEMP_DIR, javaDirectory.getAbsolutePath() );
+            JRProperties.setProperty( JRProperties.COMPILER_KEEP_JAVA_FILE, keepJava );
+            JRProperties.setProperty( JRProperties.COMPILER_CLASS, compiler );
+            JRProperties.setProperty( JRProperties.COMPILER_XML_VALIDATION, xmlValidation );
+
             Iterator it = files.iterator();
             while ( it.hasNext() )
             {
@@ -255,6 +243,8 @@ public class JasperReportsMojo
         }
         finally
         {
+            JRProperties.restoreProperties();
+
             if ( classLoader != null )
             {
                 Thread.currentThread().setContextClassLoader( classLoader );
@@ -264,10 +254,9 @@ public class JasperReportsMojo
     }
 
     /**
-     * Determines source files to be compiled, based on the SourceMapping.
-     * No longer needs to be recursive, since the SourceInclusionScanner
-     * handles that.
-     *
+     * Determines source files to be compiled, based on the SourceMapping. No longer needs to be
+     * recursive, since the SourceInclusionScanner handles that.
+     * 
      * @param mapping
      * @return
      * @throws MojoExecutionException
@@ -284,10 +273,10 @@ public class JasperReportsMojo
         {
             return scanner.getIncludedSources( sourceDirectory, outputDirectory );
         }
-        catch (InclusionScanException e)
+        catch ( InclusionScanException e )
         {
-            throw new MojoExecutionException( "Error scanning source root: \'"
-                    + sourceDirectory + "\' " + "for stale files to recompile.", e );
+            throw new MojoExecutionException( "Error scanning source root: \'" + sourceDirectory + "\' "
+                + "for stale files to recompile.", e );
         }
     }
 
@@ -366,7 +355,7 @@ public class JasperReportsMojo
                 File f = new File( element );
                 URL newURL = f.toURI().toURL();
                 classpathURLs.add( newURL );
-                getLog().debug( "Added to classpath " + element );
+                getLog().info( "Added to classpath " + element );
             }
             catch ( Exception e )
             {
@@ -385,7 +374,7 @@ public class JasperReportsMojo
                     File f = new File( element );
                     URL newURL = f.toURI().toURL();
                     classpathURLs.add( newURL );
-                    getLog().debug( "Added to classpath " + element );
+                    getLog().info( "Added to classpath " + element );
                 }
                 catch ( Exception e )
                 {
